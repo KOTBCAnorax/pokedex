66909626f8552fcb609d26fc86127095e67a9df4
KOTBCAnorax <47pohih@gmail.com>
2025-02-18 01:47:11 +0200

    Added 'help' and 'exit' commands

--- main.go
+++ main.go
@@ -7,8 +7,44 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var Commands = map[string]cliCommand{
+	"help": {
+		name:        "help",
+		description: "Get help on the Pokedex commands",
+		callback:    commandHelp,
+	},
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
 func CleanInput(text string) []string {
-	return strings.Fields(text)
+	lowerCase := strings.ToLower(text)
+	return strings.Fields(lowerCase)
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print(`Welcome to the Pokedex!
+Usage:
+
+help: Displays a help message
+exit: Exit the Pokedex
+`)
+	return nil
 }
 
 func main() {
@@ -16,9 +52,13 @@ func main() {
 	for true {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		input := scanner.Text()
-		input = strings.ToLower(input)
-		words := strings.Fields(input)
-		fmt.Printf("Your command was: %s\n", words[0])
+		inputWords := CleanInput(scanner.Text())
+		command, ok := Commands[inputWords[0]]
+		if ok {
+			err := command.callback()
+			fmt.Printf("Errors: %v\n", err)
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 }
